79d6ad1786d1e8f63773ed5471284a4a27b2a0d7
Givago Tavares <givago.mubarak1@gmail.com>
2026-03-23 15:06:27 -0300

    Project created

---
+++ src/test/java/com/givago/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givago.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
