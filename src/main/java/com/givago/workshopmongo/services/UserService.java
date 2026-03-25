70b5c59bf19b7d3a9a6465a9ddbfb070884857f8
Givago Tavares <givago.mubarak1@gmail.com>
2026-03-25 05:06:21 -0300

    obtendo um usuario por id

--- src/main/java/com/givago/workshopmongo/services/UserService.java
+++ src/main/java/com/givago/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.givago.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.givago.workshopmongo.domain.User;
 import com.givago.workshopmongo.repository.UserRepository;
+import com.givago.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return repo.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
