65a85cfcf8b72e26d406ed3861cbf245815ff18f
Givago Tavares <givago.mubarak1@gmail.com>
2026-03-26 05:58:34 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/givago/workshopmongo/resources/UserResource.java
+++ src/main/java/com/givago/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.givago.workshopmongo.resources;
 
 import java.net.URI;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.givago.workshopmongo.domain.Post;
 import com.givago.workshopmongo.domain.User;
 import com.givago.workshopmongo.dto.UserDTO;
 import com.givago.workshopmongo.services.UserService;
@@ -57,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 }
